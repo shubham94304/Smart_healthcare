ead36217e9d58ce88705e2ca763b0a635218de5c
shubham94304 <126273469+shubham94304@users.noreply.github.com>
2025-03-08 23:43:25 +0530

    urls.py

--- disease_prediction/urls.py
+++ disease_prediction/urls.py
@@ -1,4 +1,4 @@
-"""disease_prediction URL Configuration
+"""disease_prediction URL Configuration 
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
